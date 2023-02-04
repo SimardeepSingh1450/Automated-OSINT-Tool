3d888146b20c1ebe2ea28803c78c98b38cac4bdc
simardeepsm <smudhar_be21@thapar.edu>
2023-02-05 00:32:07 +0530

    some-changes-frontend

--- website/server/index.js
+++ website/server/index.js
@@ -47,25 +47,13 @@ app.get(
   require("./controllers/twitter_accounts_to_DB")
 );
 app.get("/save_twitter_tweets", require("./controllers/twitter_tweets_to_DB"));
-app.get("/save_reditcontent", require("./controllers/redit_to_DB"));
-app.get("/save_duckduckgo", require("./controllers/duckduck_go_to_DB"));
-app.get("/save_sockpuppet", require("./controllers/sockpuppet_to_DB"));
-app.get("/save_sherlock", require("./controllers/sherlock_json_to_DB"));
 
 //DB data fetching operations
 app.get(
   "/fetch_sockpuppet",
   require("./controllers/DB data fetching/sockpuppet_db")
 );
-app.get("/fetch_redit", require("./controllers/DB data fetching/redit_DB"));
-app.get(
-  "/fetch_duckduckgo",
-  require("./controllers/DB data fetching/duckduckgo_DB")
-);
-app.get(
-  "/fetch_sherlock",
-  require("./controllers/DB data fetching/sherloack_DB")
-);
+
 app.get(
   "/fetch_twitteraccounts",
   require("./controllers/DB data fetching/twitter_accounts_DB")
@@ -78,43 +66,6 @@ app.get(
 //testing repote generation
 app.get("/testing", require("./testing/repote"));
 
-//duckduckGo Operations
-const { runPythonDuck } = require("./json-code/duckduckgo-linker/index");
-app.get("/run-duck-python-scrape/:searchField", runPythonDuck);
-
-const { attainduckJson } = require("./json-code/duckduckgo-linker/index");
-app.get("/get-duck-data", attainduckJson);
-
-//googleSearch Operations
-const {
-  runGoogleScrape,
-} = require("./json-code/python-js-google-search-linker/index");
-app.get("/run-python-googleSearch/:searchField", runGoogleScrape);
-
-const {
-  fetchGoogleJson,
-} = require("./json-code/python-js-google-search-linker/index");
-app.get("/fetch-googleScrape", fetchGoogleJson);
-
-//reddit Operations
-const {
-  runRedditPythonCode,
-} = require("./json-code/python-js-reddit-linker/index");
-app.get("/run-reddit-python-scrape/:searchField", runRedditPythonCode);
-
-const { getRedditJson } = require("./json-code/python-js-reddit-linker/index");
-app.get("/get-reddit-data", getRedditJson);
-
-//sherlock Operations
-const {
-  runPythonSherlock,
-} = require("./json-code/sherlock-userid-scrapper/index");
-app.get("/python-sherlock/:userName", runPythonSherlock);
-
-const {
-  getSherlockJson,
-} = require("./json-code/sherlock-userid-scrapper/index");
-app.get("/fetch-sherlock-json", getSherlockJson);
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
