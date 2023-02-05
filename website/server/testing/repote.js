7d230a33835b5ee1f63cc66220f192191be2da10
simardeepsm <smudhar_be21@thapar.edu>
2023-02-05 11:32:38 +0530

    removed server since deployed all

--- website/server/testing/repote.js
+++
@@ -1,6 +0,0 @@
-const repotegenfun = require("./testerjs");
-const repotegenerationcontroller = async (req, res) => {
-  const data = repotegenfun();
-  res.send(data);
-};
-module.exports = repotegenerationcontroller;
